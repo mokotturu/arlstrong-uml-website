3e135e4ce039d86a375c13529d96615cddd13371
mokotturu <monishkotturu@gmail.com>
2022-07-13 14:26:51 -0400

    research outcomes

--- src/components/ResearchOutcomes.js
+++ src/components/ResearchOutcomes.js
@@ -3,16 +3,12 @@ const ResearchOutcomes = () => {
 		<section className="flex justify-center flex-1">
 			<div className="flex flex-col">
 				<div className="py-20 responsive-width child:w-full">
-					<h1 className="heading">Videos</h1>
-					<p>Videos</p>
-				</div>
-				<div className="py-20 responsive-width child:w-full">
 					<h1 className="heading">Presentations</h1>
-					<p>Presentations</p>
-				</div>
-				<div className="py-20 responsive-width child:w-full">
-					<h1 className="heading">Other Links</h1>
-					<p>Other Links</p>
+					<div className="flex flex-col w-full gap-8 child:w-full">
+						<iframe title="Alok's Presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vSXm9MzJHmFsLZfE4Sz99PlGYOhGt7-eDTjvY1nm1n7NJe0sIQh7HfY7oFG0PGl_Ako94pbVxEZp-KF/embed?start=false&loop=true&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen={true} mozallowfullscreen="true" webkitallowfullscreen="true" className="bg-neutral-100 rounded-md"></iframe>
+						<iframe title="Saba's Presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vSR04IVGFSs-Aa86DNaJ5mtQroB-xmaZHelzdEyAuTKipZPGQBHM61fPA8YAprK0NrogU51-uGl3rbT/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen={true} mozallowfullscreen="true" webkitallowfullscreen="true" className="bg-neutral-100 rounded-md"></iframe>
+						<p>View Hamid's full presentation about his work <a className="link" href="https://studentuml-my.sharepoint.com/:p:/g/personal/monishreddy_kotturu_student_uml_edu/EfmybxbBrsBHl95wO0vmzSQBYtP7LXj_VsWUFFpj39BpNw" target="_blank" rel="noopener noreferrer">here</a>.</p>
+					</div>
 				</div>
 			</div>
 		</section>
